c6409625d59b41b1d11736524d361474fa99f2c1
gustav <gh@oparko.com>
2026-03-27 13:47:11 +0100

    feat: enhance S3 website configuration with ZeroTier integration and update HTML content

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Turn the bucket into a website:
+		cfg := config.New(ctx, "")
+		zerotierNetworkID := cfg.Require("zerotierNetworkID")
+
 		website, err := NewAwsS3Website(ctx, "my-website", AwsS3WebsiteArgs{
-			Files: []string{"index.html"},
+			Files:             []string{"index.html"},
+			ZeroTierNetworkID: zerotierNetworkID,
 		})
 		if err != nil {
 			return err
 		}
 
-		// Export the name of the bucket
 		ctx.Export("bucketName", website.BucketID)
-		// Export the bucket's autoassigned URL:
-		ctx.Export("url", website.Url)
+		ctx.Export("sshAccess", website.Url) // ssh ec2-user@<public-ip>
 		return nil
 	})
 
